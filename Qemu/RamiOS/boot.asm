e927622d83f1d6cc07205051d7fd3ba9a7cf22bf
Ramisetty Ranganadh <66828499+ramisettyranganadh@users.noreply.github.com>
2025-07-02 00:29:59 +0530

    Add files via upload

--- Qemu/RamiOS/boot.asm
+++ Qemu/RamiOS/boot.asm
@@ -5,17 +5,40 @@ ORG 0x7C00
 BITS 16 
 ;This is to define the bootloader is for 16 bit architecture
 
-start:
-;start is just a label to indicate whether the actual bootloader program starts from
-    mov ah, 0eh 
-   ;Load 0eh in ah register for video out
-    mov al, 'R' 
-   ;Load Character in al register
-    mov bx,  0  
-   ;bh for page number, bl for foreground color
-    int 0x10 
-   ;int 0x10 is an interrupt to Display Characters on Terminal   jmp $
-   ;jumps itself and don't allow to write boot signature
+start:  ;start is just a label to indicate whether the actual bootloader program starts from
+	mov si, message
+	;load si with message 'Welcome to RamiOS!'
+	call print
+	; call print and return after printing message
+	jmp $
+	;jumps to itself, creates infinite loop and controls stays at that point
+
+print:  ;print is just a label like a function and definition
+	mov bx,  0  
+	;bh for page number, bl for foreground color
+.loop:  ;.loop is a sub label to loop 
+	lodsb
+	;load a character from message in sb
+	cmp al, 0
+	;compare al with 0
+	je .done
+	;jump to .done if compare al equals to 0
+	call print_char
+	;print the character loaded in sb
+	jmp .loop
+	;jump to .loop label to continue printing
+.done:  ;.done is a sub label to return
+	ret
+	;return to print
+
+print_char:
+	mov ah, 0eh 
+	;Load 0eh in ah register for video out
+	int 0x10 
+	;int 0x10 is an interrupt to Display Characters on Terminal
+	ret
+
+message: db 'Welcome to RamiOS!', 0
 
 times 510 - ($ - $$) db 0 
 ;Fill atleast 510 bytes with code and 0 to write boot signature in dw after that.
